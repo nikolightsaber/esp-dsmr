1410102e230a1898945a7ab39d8f4237e54937f2
Nikolai Devolder <nikolai.devolder@yamabiko.eu>
2025-06-26 21:59:50 +0200

    Better http server and uart begin

--- main/main.c
+++ main/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "driver/gpio.h"
+#include "driver/uart.h"
 #include "esp_event.h"
 #include "esp_http_server.h"
 #include "esp_log.h"
@@ -36,12 +37,25 @@ static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_
 esp_err_t http_404_error_handler(httpd_req_t *req, httpd_err_code_t err)
 {
     httpd_resp_set_status(req, "302 Found");
-    httpd_resp_set_hdr(req, "Location", "/");
+    httpd_resp_set_hdr(req, "Location", "/index.html");
     return httpd_resp_send(req, NULL, 0);
 }
 
-extern const uint8_t index_html_start[] asm("_binary_index_html_start");
-extern const uint8_t index_html_end[] asm("_binary_index_html_end");
+extern const char index_html_start[] asm("_binary_index_html_start");
+extern const char index_html_end[] asm("_binary_index_html_end");
+extern const char favicon_ico_start[] asm("_binary_favicon_ico_start");
+extern const char favicon_ico_end[] asm("_binary_favicon_ico_end");
+
+struct static_file {
+    char *path;
+    char *type;
+    const char *start;
+    const char *end;
+} static_files[] = {
+    { "/index.html", "text/html", index_html_start, index_html_end },
+    { "/favicon.ico", "image/x-icon", favicon_ico_start, favicon_ico_end },
+    { 0 },
+};
 
 /* An HTTP GET handler */
 static esp_err_t http_deliver_file(httpd_req_t *req)
@@ -60,12 +74,16 @@ static esp_err_t http_deliver_file(httpd_req_t *req)
         }
         free(buf);
     }
-    size_t html_len = index_html_end - index_html_start;
 
-    httpd_resp_set_type(req, "text/html");
-    httpd_resp_send(req, (const char*)index_html_start, html_len);
+    for (struct static_file *f = static_files; f->path; ++f) {
+        ESP_LOGI(TAG, "%s, %s", f->path, req->uri);
+        if (!strcmp(f->path, req->uri)) {
+            httpd_resp_set_type(req, f->type);
+            return httpd_resp_send(req, f->start, f->end - f->start);
+        }
+    }
 
-    return ESP_OK;
+    return http_404_error_handler(req, HTTPD_404_NOT_FOUND);
 }
 
 static httpd_handle_t start_webserver(void)
@@ -73,16 +91,16 @@ static httpd_handle_t start_webserver(void)
     httpd_handle_t server = NULL;
     httpd_config_t config = HTTPD_DEFAULT_CONFIG();
     config.lru_purge_enable = true;
+    config.uri_match_fn = httpd_uri_match_wildcard;
 
     ESP_LOGI(TAG, "Starting server on port: '%d'", config.server_port);
     if (httpd_start(&server, &config) == ESP_OK) {
         // Set URI handlers
         ESP_LOGI(TAG, "Registering URI handlers");
-        httpd_register_uri_handler(server, &(httpd_uri_t){
-            .uri       = "/",
-                .method    = HTTP_GET,
-                .handler   = http_deliver_file,
-                .user_ctx  = "index.html"
+        httpd_register_uri_handler(server, &(httpd_uri_t) {
+            .uri       = "/*",
+            .method    = HTTP_GET,
+            .handler   = http_deliver_file,
         });
         httpd_register_err_handler(server, HTTPD_404_NOT_FOUND, http_404_error_handler);
         return server;
@@ -128,6 +146,30 @@ void app_main(void)
     ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
     ESP_ERROR_CHECK(esp_wifi_start() );
 
+
+    uart_config_t uart_config = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity    = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_DEFAULT,
+    };
+
+    ESP_ERROR_CHECK(uart_driver_install(UART_NUM_2, 1024 * 2, 0, 0, NULL, 0));
+    ESP_ERROR_CHECK(uart_param_config(UART_NUM_2, &uart_config));
+    ESP_ERROR_CHECK(uart_set_pin(UART_NUM_2, GPIO_NUM_17, GPIO_NUM_16, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
+
+    uint8_t *data = (uint8_t *) malloc(1024);
+
+    while (1) {
+        int len = uart_read_bytes(UART_NUM_2, data, (1024 - 1), 20 / portTICK_PERIOD_MS);
+        uart_write_bytes(UART_NUM_2, (const char *) data, len);
+        if (len) {
+            data[len] = '\0';
+            ESP_LOGI(TAG, "%s", (char *) data);
+        }
+    }
     // while (1) {
     //     // ESP_LOGI(TAG, "hello");
     //     gpio_set_level(BLINK_GPIO, blink_state);
